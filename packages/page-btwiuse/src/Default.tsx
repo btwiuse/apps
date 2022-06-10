161c506a587ac09a18ce156ef271173fdf6bbef7
navigaid <navigaid@gmail.com>
2022-06-10 18:47:54 +0800

    test HelpOverlay component

--- packages/page-btwiuse/src/Default.tsx
+++ packages/page-btwiuse/src/Default.tsx
@@ -3,16 +3,29 @@
 
 import React from "react";
 
+import { HelpOverlay } from '@polkadot/react-components';
+
 interface Props {
   className?: string;
 }
 
+const helpMessage = `# btw i use arch
+
+author: [@btwiuse](https://github.com/btwiuse)
+
+oh arch
+
+my love
+
+you're the best distro in the world
+
+how am i supposed to live without you!
+`
+
 function Default({ className }: Props): React.ReactElement<Props> {
   return (
     <div className={className}>
-      <div className="ui--row">
-        <p>foo</p>
-      </div>
+      <HelpOverlay md={helpMessage} />
       <div className="ui--row">
         <p>bar</p>
       </div>
